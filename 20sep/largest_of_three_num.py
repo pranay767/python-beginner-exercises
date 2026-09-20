0074475c07270db1fa979602db5e52a1b474a163
Pranay <pranay.kirlampalli@gmail.com>
2026-09-20 19:21:49 +0530

    Eighth commit

---
+++ 20sep/largest_of_three_num.py
@@ -0,0 +1,13 @@
+def largest(num1, num2, num3):
+    if num1 > num2:
+        if num1 > num3:
+            return num1
+        else:
+            return num3
+    else:
+        if num2 > num3:
+            return num2
+        else:
+            return num3
+
+print(largest(4, 45, 19))
