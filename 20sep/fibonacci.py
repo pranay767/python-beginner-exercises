5e28fd20170f8c533874562ede5d0cc1e7cd612b
Pranay <pranay.kirlampalli@gmail.com>
2026-09-20 13:56:53 +0530

    fibonacci sequence works now

--- 20sep/fibonacci.py
+++ 20sep/fibonacci.py
@@ -1,6 +1,9 @@
 def fibonacci(limit):
     fibonacci_sequence = []
+    a,b = 0,1
     for i in range(limit):
-
+        fibonacci_sequence.append(a)
+        a,b= b, a+b
+    return fibonacci_sequence
 
 print(fibonacci(10))
